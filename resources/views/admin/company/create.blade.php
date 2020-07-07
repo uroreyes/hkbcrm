86bbfa2e469682cace66f8f88b9f9a4ec2674b7f
Mark Reyes <mark@mindstorminnovations.com>
2020-07-07 21:53:45 +0800

    create create-company form

---
+++ resources/views/admin/company/create.blade.php
@@ -0,0 +1,43 @@
+
+
+@extends('layouts.app')
+@section('content')
+<div class="container">
+   <div class="row">
+      <div class="col-sm-8 offset-sm-2">
+         <h3 class="display-3">Add Company</h3>
+         <div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+               <ul>
+                  @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                  @endforeach
+               </ul>
+            </div>
+            <br />
+            @endif
+            <form method="post" action="{{ route('admin.companies.store') }}" enctype="multipart/form-data">
+               @csrf
+               <div class="form-group">    
+                  <label for="first_name">Name:</label>
+                  <input type="text" class="form-control" name="name"/>
+               </div>
+               
+               <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="text" class="form-control" name="email"/>
+               </div>
+               <div class="form-group">
+                  <label for="photo">Attach a Logo</label>
+                  <input type="file" name="logo" id="logo" class="form-control-file">
+               </div>
+               <a href='{{  url("/admin/companies/") }}'  class="btn btn-info " style="margin-right:5px">{{ __('admin.actions.cancel') }}</a>
+               <button type="submit" class="btn btn-primary">{{ __('admin.actions.add') }}</button>
+            </form>
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
+
