4037d830a4fcade7184cf562aec297e7a4918e5e
Mark Reyes <mark@mindstorminnovations.com>
2020-07-07 21:57:13 +0800

    add actions

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -4,5 +4,13 @@ return[
     'modules' => [
         'companies' => 'Companies',
         
+    ],
+    'actions' => [
+        'add'       => 'Add',
+        'edit'      => 'Edit',
+        'delete'    => 'Delete',
+        'cancel'    => 'Cancel',
+        'view'      => 'View',
+        'save'      => 'Save',
     ]
 ];
