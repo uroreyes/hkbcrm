30b434efaf0a9ac113d45e3d145f7997e8896c4b
Mark Reyes <mark@mindstorminnovations.com>
2020-07-08 12:08:52 +0800

    add navigation for companies and employees

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,12 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('admin/companies') }}">{{ __('admin.modules.companies') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('admin/employees') }}">{{ __('admin.modules.employees') }}</a>
+                                </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
